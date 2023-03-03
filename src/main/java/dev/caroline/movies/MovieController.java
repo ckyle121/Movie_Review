2a6a1db4009512a8879c190c1a04ab231ed13926
ckyle121 <carolinekyle21@gmail.com>
2023-03-02 18:53:12 -0600

    Find movie by Id repository and controller

--- src/main/java/dev/caroline/movies/MovieController.java
+++ src/main/java/dev/caroline/movies/MovieController.java
@@ -1,15 +1,14 @@
 package dev.caroline.movies;
 
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/movies")
@@ -21,4 +20,8 @@ public class MovieController {
         return new ResponseEntity<List<Movie>>(movieService.findAllMovies(), HttpStatus.OK);
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Movie>> getSingleMovie(@PathVariable ObjectId id){
+        return new ResponseEntity<Optional<Movie>>(movieService.singleMovie(id), HttpStatus.OK);
+    }
 }
